869dd005239c42ce90f0711179482b37838359b8
Avitosh Totaram <a.totaram@gmail.com>
2019-04-27 05:20:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    return
+    return "NO, NOT SINCE 1938!"
     elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
+  elseif phrase == ""
   
   
 end
