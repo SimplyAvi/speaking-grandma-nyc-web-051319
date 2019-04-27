c97b66ba53768f1f9680843f31ded21260cc9ba5
Avitosh Totaram <a.totaram@gmail.com>
2019-04-27 05:20:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,8 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
     elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elseif phrase == ""
+  elseif phrase == "I LOVE YOU GRANDMA!"
+    return ""
   
   
 end
